35e33746e2b88d348c0ada302e72c16305f25db3
eljun <el.junsan@yahoo.com>
2016-11-24 13:34:46 +0800

    Added trello plus spreadsheet links

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,16 +1,20 @@
 var data = {
-	"ongoing": [
-		"https://trello.com/c/CbzlaxXt/1649-netlify-ember-cms-enhancement&sa=D&ust=1478593875680000&usg=AFQjCNGouKHJoJaj946FDRl-zsN07gNMRQ",
-		"https://trello.com/c/gApQR7hI/156-seasons-greetings-newsletter-2017",
-	],
-	"pending": [
-		"https://trello.com/c/xsrbVqLc/1-account-information"
-	],
 	"notes": [
 		{
 			"site": "WebriQ",
-			"info": "Revision Documentation Ok, since /accounts/create becomes our main landing page, I am wondering if we could not make the design a bit more attractive. Replace world icon with WebriQ logo. Replace follow instructions with icons of the new header image of WebriQ. Make it more attractive visually!!!" 
+			"info": "<p>Revision Documentation Ok, since /accounts/create becomes our main landing page, I am wondering if we could not make the design a bit more attractive. Replace world icon with WebriQ logo. Replace follow instructions with icons of the new header image of WebriQ. Make it more attractive visually!!!</p><p>After live, it would be useful to document this in our documentation.</p><p>NEXT WEEK: Make the landing page more impressive</p>"
+		},{
+			"site": "Webriq CMS & App",
+			"info": "<p>Please refer to your notebook</p>"
 		}
+	],
+	"ongoing": [
+		"https://trello.com/c/CbzlaxXt/1649-netlify-ember-cms-enhancement&sa=D&ust=1478593875680000&usg=AFQjCNGouKHJoJaj946FDRl-zsN07gNMRQ",
+	],
+	"pending": [
+		"https://trello.com/c/KRUIlwJq/316-blog-on-interactive-video-training",
+		"https://trello.com/c/zT9TTYYu/158-webriq-editor-features-documentation",
+		"https://trello.com/c/mi7jv9sT/39-web-service"
 	]
 }
 
@@ -20,7 +24,7 @@ var b = $('body'),
 	pending = b.find('ol.pending');
 
 $(data.notes).each(function(e, link){
-	notes.append('<li><h3>'+ link.site +'</h3><p>'+ link.info +'</p></li>')
+	notes.append('<li><h3>'+ link.site +'</h3>'+ link.info +'</li>')
 })
 
 $(data.ongoing).each(function(e, link){
